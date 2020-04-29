0f168b71dfbc5f07c2c4f099d237cb4541eb67fd
Ronaldo B Ferreyra <ronaldoferreyra@yahoo.com.br>
2020-04-28 21:04:52 -0300

    Conceito inicial da app

---
+++ public/script.js
@@ -0,0 +1,5 @@
+/**
+ * Babel: Converter (transpilar) código do React para um código que o brower entenda
+ * Webpack: Para cada tipo de arquivo (.js .css .png) eu vou converter o código de uma maneira diferente
+ * - Loaders: babel-loader, css-loader, image-loader, etc...
+ */
